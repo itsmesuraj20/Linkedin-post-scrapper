4e9e9427ba0b2bfc666a26c1044d0e3d34c038f0
SURAJ SINGH <rameshwarsingh99351@gmail.com>
2025-09-10 23:30:48 +0530

    adding DB (used DB : MongoDB)

---
+++ linkedin-scraper/server/logger.ts
@@ -0,0 +1,7 @@
+import morgan from 'morgan';
+import fs from 'fs';
+import path from 'path';
+
+const logStream = fs.createWriteStream(path.join(__dirname, 'access.log'), { flags: 'a' });
+
+export const logger = morgan('combined', { stream: logStream });
